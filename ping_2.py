9378d1a1bb073409cfca08980d17d2cc7b4d8591
Rafael Morais <35689807+faelmoraiz@users.noreply.github.com>
2018-06-28 08:55:32 -0300

    Envio de e-mail atualizado
    Envio de e-mail atualizado

--- ping_2.py
+++ ping_2.py
@@ -3,40 +3,42 @@ import subprocess, time, os, getpass, smtplib
 
 os.system('cls')
 
-pingAlto = 8
-intervalo = 1 # segundos
-dadosLimite = 30 # limite de armazenamento de dados
+pingAlto = 100 # Tempo máximo de resposta / Alerta por e-mail.
+intervalo = 1 # Intervalo em segundos entre as requisições.
+dadosLimite = 30 # limite de armazenamento de dados.
 
-emailOrigem = '...@gmail.com'
-emailDestino = '...@....com'
+emailOrigem = 'seuemail@gmail.com'
+emailDestino = 'destino@email'
 
 def sendmail(toaddr, msg, passwd):
-	relogio='['+time.strftime('%H:%M:%S')+']'
-	fromaddr = emailOrigem
-	username = fromaddr
-	password = passwd
-	server = smtplib.SMTP('smtp.gmail.com:587')
-	server.starttls()
-	server.login(username,password)
-	content= '\r\n'.join([
-			'From: %s' % fromaddr,
-			'To: %s' % toaddr,
-			'Subject: %s' % msg,
-			'',
-			'Enviado as %s por ping.py' % relogio
-			])
-	server.sendmail(fromaddr, toaddr, content)
-	print('\nEmail enviado.')
-	server.quit()
+	try:
+		relogio='['+time.strftime('%H:%M:%S')+']'
+		fromaddr = emailOrigem
+		username = fromaddr
+		password = passwd
+		server = smtplib.SMTP('smtp.gmail.com:587')
+		server.starttls()
+		server.login(username,password)
+		content= '\r\n'.join([
+				'From: %s' % fromaddr,
+				'To: %s' % toaddr,
+				'Subject: %s' % msg,
+				'',
+				'Enviado as %s por ping.py' % relogio
+				])
+		server.sendmail(fromaddr, toaddr, content)
+		print('\nEmail enviado.')
+		server.quit()
+	except:
+		pass
 
 def ping(ip, passwd):
-	lst=[]
+	lst=[];pingsAltos=[]
 	try:
-		r=open(ip.replace('.','_')+'.txt', 'r')
-		for x in r:
-			x=x.strip()
-			lst.append(float(x))
-		r.close()
+		with open(ip.replace('.','_')+'.txt', 'r') as r:
+			for x in r:
+				x=x.strip()
+				lst.append(float(x))
 	except Exception as e:
 		# print(e)
 		pass
@@ -53,11 +55,21 @@ def ping(ip, passwd):
 			print('\n[!] Ping: {:.2f}ms'.format(float(a)))
 			print('[/] Média: {:.2f}ms'.format(sum(lst)/len(lst)))
 			print('[#] Dados:', str(len(lst))+'/'+str(dadosLimite))
+			with open(ip.replace('.','_')+'.txt', 'r') as r:
+				tmp_lst = []
+				for x in r:
+					x=x.strip()
+					tmp_lst.append(float(x))
+				print('\n[+] Média geral: {:.2f}\n'.format(sum(tmp_lst)/len(tmp_lst)))
 			if (sum(lst)/len(lst))>pingAlto:
-				sendmail(emailDestino, '# PING ALTO: {:.2f}ms - [{}]'.format(float(a),ip), passwd )
+				sendmail(emailDestino, '# PING ALTO: {:.2f}ms - [{}]'.format(float(sum(lst)/len(lst)),ip), passwd )
+				pingsAltos.append('['+time.strftime('%H:%M:%S')+']'+' -> '+str(float(a))+'ms.')
 				lst=[]
 				time.sleep(10)
 			if len(lst)>=dadosLimite:lst=[]
+			if len(pingsAltos)>0:
+				for _ping in pingsAltos:
+					print('[!]', _ping)
 		except Exception as e:
 			print('Não foi possível pingar no endereço:', ip)
 			print('Erro:', e)
